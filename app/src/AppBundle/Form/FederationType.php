2fbca83e1f35b4a53ee4860dfcf0a5738d479bc9
Tóth Béla <tothbela@mta.sztaki.hu>
2019-05-06 17:20:53 +0200

    federation create view

--- app/src/AppBundle/Form/FederationType.php
+++ app/src/AppBundle/Form/FederationType.php
@@ -2,7 +2,9 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +15,9 @@ class FederationType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('metadataUrl')->add('lastChecked')->add('name')->add('slug')->add('federationUrl')->add('contactName')->add('contactEmail')->add('sps')->add('idps');
+        $builder->add('metadataUrl')->add('lastChecked' , DateTimeType::class, array('disabled'=> true, 'widget' => 'single_text', 'format' => 'yyyy-MM-dd  HH:mm'))->add('name')->add('slug')->add('federationUrl')->add('contactName')->add('contactEmail')->add('sps' ,
+                IntegerType::class,
+                array('disabled'=> true))->add('idps');
     }
     /**
      * {@inheritdoc}
